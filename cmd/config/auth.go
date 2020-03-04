869b3f9e4430ec49c145a526e3dbfee338e70b1b
Duco van Amstel <helcaraxan@gmail.com>
2020-03-04 23:12:37 +0000

    Support token auth

--- cmd/config/auth.go
+++ cmd/config/auth.go
@@ -12,20 +12,22 @@ import (
 )
 
 type AuthConfig struct {
-	PubKey   *string       `yaml:"pub_key,omitempty"`
-	UserPass *UserPassword `yaml:"userpass,omitempty"`
+	PubKey      *string       `yaml:"pub_key,omitempty"`
+	TokenEnvVar *string       `yaml:"token_envvar,omitempty"`
+	UserPass    *UserPassword `yaml:"userpass,omitempty"`
 }
 
 type UserPassword struct {
-	Username       string `yaml:"username,omitempty"`
-	PasswordFile   string `yaml:"password_file,omitempty"`
-	PasswordEnvVar string `yaml:"password_envvar,omitempty"`
+	Username     string `yaml:"username,omitempty"`
+	PasswordFile string `yaml:"password_file,omitempty"`
 }
 
 func (a AuthConfig) ExtractAuth() (transport.AuthMethod, error) {
 	switch {
 	case a.PubKey != nil:
 		return a.extractAuthSSH()
+	case a.TokenEnvVar != nil:
+		return a.extractAuthToken()
 	case a.UserPass != nil:
 		return a.extractAuthUserPass()
 	default:
@@ -45,28 +47,26 @@ func (a AuthConfig) extractAuthSSH() (transport.AuthMethod, error) {
 	return publicKey, nil
 }
 
+func (a AuthConfig) extractAuthToken() (transport.AuthMethod, error) {
+	token, ok := os.LookupEnv(*a.TokenEnvVar)
+	if !ok {
+		return nil, fmt.Errorf("authentication environment variable %q was not set", *a.TokenEnvVar)
+	}
+	return &http.TokenAuth{Token: token}, nil
+}
+
 func (a AuthConfig) extractAuthUserPass() (transport.AuthMethod, error) {
-	c := a.UserPass
-	if c.Username == "" || (c.PasswordEnvVar == "" && c.PasswordFile == "") {
+	if a.UserPass.Username == "" || a.UserPass.PasswordFile == "" {
 		return nil, errors.New("no username and / or password source configured")
 	}
 
-	var token string
-	if c.PasswordEnvVar != "" {
-		var ok bool
-		if token, ok = os.LookupEnv(c.PasswordEnvVar); !ok {
-			return nil, fmt.Errorf("authentication environment variable %q was not set", c.PasswordEnvVar)
-		}
-	} else {
-		b, err := ioutil.ReadFile(a.UserPass.PasswordFile)
-		if err != nil {
-			return nil, err
-		}
-		token = string(b)
+	token, err := ioutil.ReadFile(a.UserPass.PasswordFile)
+	if err != nil {
+		return nil, err
 	}
 
 	return &http.BasicAuth{
 		Username: a.UserPass.Username,
-		Password: token,
+		Password: string(token),
 	}, nil
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	github.com/google/go-cmp v0.4.0
 	github.com/rogpeppe/go-internal v1.5.2
 	github.com/sirupsen/logrus v1.4.2
-	github.com/spf13/cobra v0.0.5
+	github.com/spf13/cobra v0.0.6
 	golang.org/x/mod v0.2.0
 	gopkg.in/src-d/go-billy.v4 v4.3.2
 	gopkg.in/src-d/go-git.v4 v4.13.1
